041884d57bd5914eaf3857069cfe8cfb4a58dc2b
Alexandre Bianchi <alexandre.bianchi@ab-inbev.com>
2020-11-29 13:22:56 -0300

    First Commit

---
+++ BondVillains/VillainCollectionViewController.swift
@@ -0,0 +1,69 @@
+//
+//  VillainCollectionViewController.swift
+//  BondVillains
+//
+//  Created by Gabrielle Miller-Messner on 2/3/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - VillainCollectionViewController: UICollectionViewController
+
+class VillainCollectionViewController: UICollectionViewController {
+    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    // MARK: Properties
+    
+    // Get ahold of some villains, for the table
+    // This is an array of Villain instances
+    let allVillains = Villain.allVillains
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let space:CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: Table View Data Source
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.allVillains.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "VillainCollectionViewCell", for: indexPath) as! VillainCollectionViewCell
+        let villain = self.allVillains[(indexPath as NSIndexPath).row]
+        
+        // Set the name and image
+        cell.nameLabel.text = villain.name
+        cell.villainImageView?.image = UIImage(named: villain.imageName)
+        //cell.schemeLabel.text = "Scheme: \(villain.evilScheme)"
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "VillainDetailViewController") as! VillainDetailViewController
+        detailController.villain = self.allVillains[(indexPath as NSIndexPath).row]
+        self.navigationController!.pushViewController(detailController, animated: true)
+        
+    }
+    
+}
